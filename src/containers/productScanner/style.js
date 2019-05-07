90bc4a8f3d3b9ce3a6c86617f5f03159ba6e58bb
Askhat <baltabaev.a2509@gmail.com>
2019-05-07 17:41:55 +0600

    fix(prodcut-scanner): size

--- src/containers/productScanner/style.js
+++ src/containers/productScanner/style.js
@@ -10,12 +10,13 @@ export const Wrap = styled.div`
 
 export const ScannerWrap = styled.div`
   position: relative;
+  height: 165px;
   margin: 50px 10px;
   border: 4px solid #fff;
 `;
 
 export const Text = styled.div`
-  margin-top: 120px;
+  margin-top: 100px;
   color: #fff;
 `;
 
