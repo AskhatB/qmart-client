d6806e7c7a6994caebe8a9bdeda04b5a189b44f1
Askhat <baltabaev.a2509@gmail.com>
2019-05-19 18:49:59 +0600

    fix: link to scanner

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -29,7 +29,7 @@ class Camera extends React.Component {
     try {
       const imageSrc = this.webcam.getScreenshot();
       const imageBase64 = imageSrc.split(',')[1];
-      const data = await axios.post('https://qmart-scanner.unie.kz/', {
+      const data = await axios.post('https://scanner.qmart.xyz/', {
         image: imageBase64
       });
       if (data.data !== 'Not recognized') {
